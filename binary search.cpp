0c890182457f5ae5f96fdfc67560fd4bf6b70abe
arjunsivadas <48342708+arjunsivadas@users.noreply.github.com>
2019-03-08 10:00:37 +0530

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,37 @@
+main()
+{
+int arr[20],n,search,first,last,mid,i;
+printf("enter the number of element");
+scanf("%d",&n);
+printf("enter the array element")
+for(i=0;i<n,i++)
+{
+scanf("%d",&arr[1]);
+}
+printf("enter search element");
+scanf("%d",&search);
+first=0;
+last=n-1;
+mid=(first->last)/2
+while(first<=last)
+{
+int(arr[mid]<search
+{
+first=mid+1;
+else if(arr[mid]=0=search;
+{
+    printf("%d found at location %d/n"search,mid+1);
+    break;
+}
+}
+}
+
+else
+    last=middle-1;
+    middle=(first+last)/2
+}
+if(first>last)
+{
+    printf("not found"%d is not present in list/n",search)
+    return o;
+}
